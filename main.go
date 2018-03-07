8e7c0ab9525f2139a5fb58f5e536d7878566afb8
wangning <416621723@qq.com>
2018-03-08 01:31:40 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "agora_blog/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
